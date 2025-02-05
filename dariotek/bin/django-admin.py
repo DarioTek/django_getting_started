707cea624ee8795b7b9ad61e1226db0d631cefc8
DarioTek <dmontecastro@yahoo.com>
2025-02-04 19:56:59 -0500

    baseline

---
+++ dariotek/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/dariomontecastro/git/django_getting_started/dariotek/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
